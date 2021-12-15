530dc6709201400d8dd3aff60aa3326743e96dc5
PRIYANSHU-2002 <77874876+PRIYANSHU-2002@users.noreply.github.com>
2021-12-15 19:00:41 +0530

    Add files via upload

--- GREATEST.java
+++ GREATEST.java
@@ -1,31 +1,31 @@
 // THIS PROGRAM IS MADE BY PRIYANSHU SINGH 
 
 import java.util.*;
-import java.lang.Math;
 public class GREATEST {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-Scanner input = new Scanner(System.in);
-System.out.println("ENTER NUMBER 1 ");
-int a = input.nextInt();
-System.out.println("ENTER NUMBER 2 ");
-int b = input.nextInt();
-System.out.println("ENTER NUMBER 3 ");
-int c = input.nextInt();
-
-int d = Math.max(a, b);   // COMPARING a AND b AND STORING THE LARGER NUMBER IN d
-int e = Math.max(d,c);    // COMPARING d AND c AND STORING THE LARGER NUMBER IN e
-
-// THE LARGEST NUMBER GETS STORED IN e 
-if (a==b && a==c)
-{
-System.out.println("ALL THE THREE NUMBERS ARE EQUAL .");
-}
-else
-{
-System.out.println(e+" IS THE LARGEST NUMBER .");
-}
+     Scanner inp = new Scanner(System.in);
+     System.out.println(" ENTER NUMBER 1 ");
+     int a = inp.nextInt();
+     System.out.println(" ENTER NUMBER 2 ");
+     int b = inp.nextInt();
+     System.out.println(" ENTER NUMBER 3 ");
+     int c = inp.nextInt();
+     
+     if(a>b && a>c)
+     {
+    	 System.out.println(a+" IS THE GREATEST AMONG ALL THE THREE NUMBERS .");
+     }
+     else if(b>a && b>c)
+     {
+    	 System.out.println(b+" IS THE GREATEST AMONG ALL THE THREE NUMBERS .");
+     }
+     else
+     {
+    	 System.out.println(c+" IS THE GREATEST AMONG ALL THE THREE NUMBERS .");
+     }
+     
 	}
 
 }
--- SMALLEST.java
+++ SMALLEST.java
@@ -1,32 +1,36 @@
 // THIS PROGRAM IS MADE BY PRIYANSHU SINGH 
 
-import java.util.*;
-import java.lang.Math;
+import java.util.Scanner;
+
 public class SMALLEST {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-Scanner input = new Scanner(System.in);
-System.out.println("ENTER NUMBER 1 ");
-int a = input.nextInt();
-System.out.println("ENTER NUMBER 2 ");
-int b = input.nextInt();
-System.out.println("ENTER NUMBER 3 ");
-int c = input.nextInt();
+		 Scanner inp = new Scanner(System.in);
+	     System.out.println(" ENTER NUMBER 1 ");
+	     int a = inp.nextInt();
+	     System.out.println(" ENTER NUMBER 2 ");
+	     int b = inp.nextInt();
+	     System.out.println(" ENTER NUMBER 3 ");
+	     int c = inp.nextInt();
+	     
+	     if(a<b && a<c)
+	     {
+	    	 System.out.println(a+" IS THE SMALLEST AMONG ALL THE THREE NUMBERS .");
+	     }
+	     else if(b<a && b<c)
+	     {
+	    	 System.out.println(b+" IS THE SMALLEST AMONG ALL THE THREE NUMBERS .");
+	     }
+	     else if(c<a && c<b)
+	     {
+	    	 System.out.println(c+" IS THE SMALLEST AMONG ALL THE THREE NUMBERS .");
+	     }
+	     else
+	     {
+	    	 System.out.println(c+" ALL THE THREE NUMBERS ARWE EQUAL.");
+	     }
 
-int d = Math.min(a, b);   // COMPARING a AND b AND STORING THE SMALLER NUMBER IN d
-int e = Math.min(d,c);    // COMPARING d AND c AND STORING THE SMALLER NUMBER IN e
-
-// THE SMALLEST NUMBER GETS STORED IN e 
-if (a==b && a==c)
-{
-System.out.println("ALL THE THREE NUMBERS ARE EQUAL .");
-}
-else
-{
-System.out.println(e+" IS THE SMALLEST NUMBER .");
-}
 	}
 
 }
-
